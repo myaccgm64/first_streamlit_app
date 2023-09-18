2bb52c3fe2e35aafb8c20bcaa94fb0cb0b4403bc
myaccgm64 <145082606+myaccgm64@users.noreply.github.com>
2023-09-18 15:26:50 -0400

    Update streamlit_app.py
    Updated for my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-import streamlit
+import streamlit as st
+import pandas as pd
 
 streamlit.little('My parents new healthy dinner')
 
@@ -8,3 +9,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
